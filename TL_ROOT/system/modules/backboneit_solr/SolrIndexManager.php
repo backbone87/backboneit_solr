0b3a67c0f6855b3c19e2bb02a9147eaf36c51545
Oliver Hoff <oliver@hofff.com>
2012-06-05 21:02:24 +0200

    simplified the index manager (aligned with search source manager)

--- TL_ROOT/system/modules/backboneit_solr/SolrIndexManager.php
+++ TL_ROOT/system/modules/backboneit_solr/SolrIndexManager.php
@@ -8,54 +8,12 @@ final class SolrIndexManager extends SolrAbstractIndexFactory {
 	
 	protected $arrFactories = array();
 	
-	protected $arrIndexes = array();
-	
-	public function add($varIndex) {
-		if(is_callable($varIndex)) {
-			$varIndex = call_user_func($varIndex);
-			
-		} elseif(is_string($varIndex)) {
-			try {
-				$objClass = new ReflectionClass($varIndex);
-				if($objClass->isSubclassOf('SolrIndexFactory')) {
-					if($objClass->hasMethod('getInstance')) {
-						$varIndex = $objClass->getMethod('getInstance')->invoke(null);
-					} else {
-						$varIndex = $objClass->newInstance();
-					}
-					
-				} elseif($objClass->isSubclassOf('SolrIndex')) {
-					if($objClass->hasMethod('getInstance')) {
-						$varIndex = $objClass->getMethod('getInstance')->invoke(null);
-					} else {
-						$varIndex = $objClass->newInstance();
-					}
-				}
-			} catch (LogicException $e) {
-				throw $e; // TODO correct exception handling
-			} catch (ReflectionException $e) {
-				throw $e; // TODO correct exception handling
-			}
-		}
-		
-		if(is_object($varIndex)) {
-			if($varIndex instanceof SolrIndexFactory) {
-				$this->arrFactories[spl_object_hash($objFactory)] = $objFactory;
-				return true;
-			} elseif($varIndex instanceof SolrIndex) {
-				$this->arrIndexes[spl_object_hash($objIndex)] = $objIndex;
-				return true;
-			}
-		}
-		
-		return false;
+	public function add(SolrIndexFactory $objFactory) {
+		$this->arrFactories[spl_object_hash($objFactory)] = $objFactory;
 	}
 	
-	public function remove($varIndex) {
-		if(is_object($varIndex)) {
-			$strHash = spl_object_hash($varIndex);
-			unset($this->arrFactories[$strHash], $this->arrIndexes[$strHash]);
-		}
+	public function remove(SolrIndexFactory $objFactory) {
+		unset($this->arrFactories[spl_object_hash($objFactory)]);
 	}
 	
 	public function runUpdates($blnScheduled = true) {
@@ -69,46 +27,32 @@ final class SolrIndexManager extends SolrAbstractIndexFactory {
 	}
 	
 	public function getIndexNames() {
-		$arrNames = array(array_keys($this->arrIndexes));
-		
+		$arrNames = array();
 		foreach($this->arrFactories as $objFactory) {
 			$arrNames[] = $objFactory->getIndexNames();
 		}
-		
 		return $arrNames ? call_user_func_array('array_merge', $arrNames) : array();
 	}
 	
 	public function getIndexes() {
-		$arrIndexes = array($this->arrIndexes);
-		
+		$arrIndexes = array();
 		foreach($this->arrFactories as $objFactory) {
 			$arrIndexes[] = $objFactory->getIndexes();
 		}
-		
 		return $arrIndexes ? call_user_func_array('array_merge', $arrIndexes) : $arrIndexes;
 	}
 	
 	public function getIndex($strName) {
-		if(isset($this->arrIndexes[$strName])) {
-			return $this->arrIndexes[$strName];
-		}
-		
 		foreach($this->arrFactories as $objFactory) if($objFactory->hasIndex($strName)) {
 			return $objFactory->getIndex($strName);
 		}
-		
 		return null;
 	}
 	
 	public function hasIndex($strName) {
-		if(isset($this->arrIndexes[$strName])) {
-			return true;
-		}
-		
 		foreach($this->arrFactories as $objFactory) if($objFactory->hasIndex($strName)) {
 			return true;
 		}
-		
 		return false;
 	}
 	
