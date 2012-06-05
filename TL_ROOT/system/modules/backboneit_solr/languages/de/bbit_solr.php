9fa1cc7643f50df7f82da49e88807e4160821c84
Oliver Hoff <oliver@hofff.com>
2012-06-05 22:36:12 +0200

    lang folder name corrected

--- TL_ROOT/system/modules/backboneit_solr/languages/de/bbit_solr.php
+++ TL_ROOT/system/modules/backboneit_solr/languages/de/bbit_solr.php
@@ -1,5 +1,5 @@
 <?php
-var_dump('bbit_solr');
+
 $GLOBALS['TL_LANG']['bbit_solr']['runUpdates'] = 'Solr Suchindexe wurden aktualisiert.';
 $GLOBALS['TL_LANG']['bbit_solr']['index']['default'] = 'Standardindex';
 $GLOBALS['TL_LANG']['bbit_solr']['source']['bbit_solr_page'] = 'Contao Seiten';
