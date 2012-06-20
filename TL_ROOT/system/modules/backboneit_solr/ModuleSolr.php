f9a388537d5b01635090fe7bae169465f5a1ebf1
Oliver Hoff <oliver@hofff.com>
2012-06-21 01:45:19 +0200

    removed exit for debugged

--- TL_ROOT/system/modules/backboneit_solr/ModuleSolr.php
+++ TL_ROOT/system/modules/backboneit_solr/ModuleSolr.php
@@ -10,8 +10,8 @@ class ModuleSolr extends BackendModule {
 		SolrIndexManager::getInstance()->runUpdates(false);
 		$this->loadLanguageFile('bbit_solr');
 		$_SESSION['TL_INFO']['bbit_solr_runUpdates'] = $GLOBALS['TL_LANG']['bbit_solr']['runUpdates'];
-// 		$this->redirect($this->getReferer());
-		exit;
+		$this->redirect($this->getReferer());
+// 		exit;
 	}
 	
 	protected function compile() {
